4b15628d638ad9f0e134e718517920b0773b1de7
ariamansardmohkam-hub <ariamansardmohkam@gmail.com>
2026-06-17 23:22:59 +0330

    Update 880cp.js

--- 880cp.js
+++ 880cp.js
@@ -1,15 +1,18 @@
-function doPost(e) {
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+<button onclick="sendOrder()">خرید</button>
 
-  var data = JSON.parse(e.postData.contents || "{}");
+<script>
+async function sendOrder() {
+  const data = {
+    uid: prompt("UID خود را وارد کنید"),
+    username: prompt("Username را وارد کنید"),
+    cp: 420
+  };
 
-  sheet.appendRow([
-    data.uid,
-    data.username,
-    data.cp,
-    new Date()
-  ]);
+  const res = await fetch("https://script.google.com/macros/s/AKfycbwpjrFwfMJ7eiS8VmqQW9rm-cfjeB7PHBNb-4nQWeruVZMjzzZcLcpxnEUGxAD322D5iQ/exec", {
+    method: "POST",
+    body: JSON.stringify(data)
+  });
 
-  return ContentService
-    .createTextOutput("OK");
+  alert("سفارش ثبت شد");
 }
+</script>
