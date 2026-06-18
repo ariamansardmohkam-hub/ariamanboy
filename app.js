ad97160ce7e820ef6fab97bec1b868c962c5ca71
ariamansardmohkam-hub <ariamansardmohkam@gmail.com>
2026-06-18 23:20:33 +0330

    Create app.js

---
+++ app.js
@@ -0,0 +1,35 @@
+const rate = 89000 / 80;
+
+let input = document.getElementById("cp");
+let priceText = document.getElementById("price");
+
+input.addEventListener("input", updatePrice);
+
+function updatePrice(){
+    let cp = parseInt(input.value);
+
+    if(!cp || cp <= 0){
+        priceText.innerText = "قیمت: 0 تومان";
+        return;
+    }
+
+    let price = Math.round(cp * rate);
+
+    priceText.innerText = "قیمت: " + price.toLocaleString() + " تومان";
+}
+
+function setCP(val){
+    input.value = val;
+    updatePrice();
+}
+
+function buy(){
+    let cp = parseInt(input.value);
+
+    if(!cp || cp <= 0){
+        alert("اول مقدار CP را وارد کن");
+        return;
+    }
+
+    window.location.href = "comingsoon.html";
+}
